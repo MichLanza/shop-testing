9de053b1ade90a605e8167f22914c3c4bfa3d51a
Michele Lanza <michele_lanza1@hotmail.com>
2025-05-09 22:04:21 -0400

    feat: implement authentication and authorization features with guards and interceptors

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -12,6 +13,9 @@ export class LoginPageComponent {
   hasError = signal(false);
   isPosting = signal(false);
 
+  router = inject(Router);
+
+  authService = inject(AuthService);
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
@@ -32,5 +36,16 @@ export class LoginPageComponent {
 
     console.log({ email, password });
 
+    this.authService.login(email!, password!)
+      .subscribe(isAuthenticated => {
+        if (isAuthenticated) {
+          this.router.navigateByUrl('/');
+          return;
+        }
+        this.hasError.set(true);
+        setTimeout(() => {
+          this.hasError.set(false);
+        }, 2000);
+      });
   }
 }
